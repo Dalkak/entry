f237576a88024f7961d0eafe1381d8bd6eae14fc
Gnlow <entrydark@gmail.com>
2020-02-27 21:16:11 +0900

    Add: when_run_button_click event

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import {
     Extension,
     Block,
+    Event,
 } from "dalkak";
 
 import message_cast from "./blocks/message_cast";
@@ -10,5 +11,13 @@ export default new Extension({
     color: 0x00B400, // Background color of blocks
     blocks: {
         message_cast
+    },
+    events: {
+        when_run_button_click: new Event("when_run_button_click")
+    },
+    on: {
+        run(){
+            this.events.when_run_button_click.fire();
+        }
     },
 });

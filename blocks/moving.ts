035fdc57b38926f5101e9195333b21ac35b5c58d
Gnlow <entrydark@gmail.com>
2020-03-04 21:12:01 +0900

    Add moving blocks

---
+++ blocks/moving.ts
@@ -0,0 +1,53 @@
+import { Block, Thing } from "dalkak";
+import basic from "@dalkak/basic";
+
+export const move_x = new Block({
+    pack: basic,
+    name: "move_x",
+    template: "x 좌표를 (value: number)만큼 바꾸기 (_target: Thing)",
+    func: ({value, _target}: {value: number, _target: Thing}) => {
+        _target.pos.x += value;
+    }
+});
+export const move_y = new Block({
+    pack: basic,
+    name: "move_y",
+    template: "y 좌표를 (value: number)만큼 바꾸기 (_target: Thing)",
+    func: ({value, _target}: {value: number, _target: Thing}) => {
+        _target.pos.y += value;
+    }
+});
+export const locate_x = new Block({
+    pack: basic,
+    name: "locate_x",
+    template: "x: (value: number) 위치로 이동하기 (_target: Thing)",
+    func: ({value, _target}: {value: number, _target: Thing}) => {
+        _target.pos.x = value;
+    }
+});
+export const locate_y = new Block({
+    pack: basic,
+    name: "locate_y",
+    template: "y: (value: number) 위치로 이동하기 (_target: Thing)",
+    func: ({value, _target}: {value: number, _target: Thing}) => {
+        _target.pos.y = value;
+    }
+});
+export const locate_xy = new Block({
+    pack: basic,
+    name: "locate_xy",
+    template: "x: (xValue: number) y: (yValue: number) 위치로 이동하기 (_target: Thing)",
+    func: ({xValue, yValue, _target}: {xValue: number, yValue: number, _target: Thing}) => {
+        _target.pos.x = xValue;
+        _target.pos.y = yValue;
+    }
+});
+export const locate = new Block({
+    pack: basic,
+    name: "locate",
+    template: "(value: Thing) 위치로 이동하기 (_target: Thing)",
+    func: ({value, _target}: {value: Thing, _target: Thing}) => {
+        _target.pos.x = value.pos.x;
+        _target.pos.y = value.pos.y;
+    }
+});

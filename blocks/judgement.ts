b64c5d2dd4b933fd467a1d67d2689f9e5f87bbe7
Gnlow <entrydark@gmail.com>
2020-03-04 21:52:02 +0900

    Update judgement.ts

--- blocks/judgement.ts
+++ blocks/judgement.ts
@@ -3,7 +3,7 @@ import { Block } from "dalkak";
 export const boolean_basic_operator = new Block({
     name: "boolean_basic_operator",
     template: "<(a) (operator) (b)>",
-    func: ({a, operator, b}, project) => {
+    func: ({a, operator, b}) => {
         a = Number(a) || a;
         b = Number(b) || b;
         switch(operator){
@@ -18,5 +18,24 @@ export const boolean_basic_operator = new Block({
             case "LESS_OR_EQUAL":
                 return a <= b;
         }
+    }
+});
+export const boolean_and_or = new Block({
+    name: "boolean_and_or",
+    template: "<<a> (operator) <b>>",
+    func: ({a, operator, b}) => {
+        switch(operator){
+            case "AND":
+                return a && b;
+            case "OR":
+                return a || b;
+        }
+    }
+});
+export const boolean_not = new Block({
+    name: "boolean_not",
+    template: "<<a> 가 아니다>",
+    func: ({a}) => {
+        return !a;
     }
 });

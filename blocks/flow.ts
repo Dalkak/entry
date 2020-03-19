b3f18237f98c296b41dbe86acbf0aa7e018cb74b
Gnlow <entrydark@gmail.com>
2020-03-19 22:46:41 +0900

    update dalkak

--- blocks/flow.ts
+++ blocks/flow.ts
@@ -3,7 +3,7 @@ import { Block } from "dalkak";
 export const wait_second = new Block({
     name: "wait_second",
     template: "(second) 초 기다리기",
-    func: async ({second}, project, platform) => {
+    func: async ({second}, project, local) => {
         await new Promise((o, x) => {
             setTimeout(o, second * 1000);
         });
@@ -12,25 +12,25 @@ export const wait_second = new Block({
 export const repeat_basic = new Block({
     name: "repeat_basic",
     template: "{ (n) 번 반복하기 {code} }",
-    func: async ({n, code}, project, platform) => {
+    func: async ({n, code}, project, local) => {
         for(var i=0;i<n;i++){
-            await code.run(project, platform);
+            await code.run(project, local);
         }
     }
 });
 export const repeat_inf = new Block({
     name: "repeat_inf",
     template: "{ 계속 반복하기 {code} }",
-    func: async ({code}, project, platform) => {
-        setInterval(async () => await code.run(project, platform));
+    func: async ({code}, project, local) => {
+        setInterval(async () => await code.run(project, local));
     }
 });
 export const repeat_while_true = new Block({
     name: "repeat_while_true",
     template: "{ <state>이 될 때까지 반복하기 {code} }",
-    func: async ({state, code}, project, platform) => {
+    func: async ({state, code}, project, local) => {
         let loop = setInterval(async () => {
-            await code.run(project, platform);
+            await code.run(project, local);
             if(state){
                 clearInterval(loop);
             }
@@ -40,27 +40,27 @@ export const repeat_while_true = new Block({
 export const _if = new Block({
     name: "_if",
     template: "{ 만일 <state>라면 {code} }",
-    func: async ({state, code}, project, platform) => {
+    func: async ({state, code}, project, local) => {
         if(state){
-            await code.run(project, platform);
+            await code.run(project, local);
         }
     }
 });
 export const if_else = new Block({
     name: "if_else",
     template: "{ 만일 <state>라면 {code1} 아니면 {code2} }",
-    func: async ({state, code1, code2}, project, platform) => {
+    func: async ({state, code1, code2}, project, local) => {
         if(state){
-            await code1.run(project, platform);
+            await code1.run(project, local);
         }else{
-            await code2.run(project, platform);
+            await code2.run(project, local);
         }
     }
 });
 export const wait_until_true = new Block({
     name: "wait_until_true",
     template: "{ <state>이 될 때까지 기다리기 }",
-    func: async ({state}, project, platform) => await new Promise( (o, x) => {
+    func: async ({state}, project, local) => await new Promise( (o, x) => {
         let loop = setInterval(() => {
             if(state){
                 clearInterval(loop);
--- blocks/variable.ts
+++ blocks/variable.ts
@@ -4,107 +4,107 @@ import basic from "@dalkak/basic"
 export const get_variable = new Block({
     name: "get_variable",
     template: "((name) 값)",
-    func: ({name}, project) => {
-        if(!project.variables.value[name]){
-            project.variables.value[name] = new Variable({name, value: 0});
+    func: ({name}, project, {variables}) => {
+        if(!variables.value[name]){
+            variables.value[name] = new Variable({name, value: 0});
         }
-        return project.variables.value[name].value;
+        return variables.value[name].value;
     }
 });
 export const change_variable = new Block({
     pack: basic,
     name: "change_variable",
     template: "{(name)에 (value: number)만큼 더하기}",
-    func: ({name, value}, project) => {
-        if(!project.variables.value[name]){
-            project.variables.value[name] = new Variable({name, value: 0});
+    func: ({name, value}, project, {variables}) => {
+        if(!variables.value[name]){
+            variables.value[name] = new Variable({name, value: 0});
         }
-        project.variables.value[name].value += Number(value);
+        variables.value[name].value += Number(value);
     }
 });
 export const set_variable = new Block({
     pack: basic,
     name: "set_variable",
     template: "{(name)를 (value)로 정하기}",
-    func: ({name, value}, project) => {
-        if(!project.variables.value[name]){
-            project.variables.value[name] = new Variable({name, value: 0});
+    func: ({name, value}, project, {variables}) => {
+        if(!variables.value[name]){
+            variables.value[name] = new Variable({name, value: 0});
         }
-        project.variables.value[name].value = value;
+        variables.value[name].value = value;
     }
 });
 export const value_of_index_from_list = new Block({
     pack: basic,
     name: "value_of_index_from_list",
     template: "((name)의 (index: number)번째 항목)",
-    func: ({name, index}, project) => {
-        if(!project.variables.value[name]){
-            project.variables.value[name] = new Variable({name, value: []});
+    func: ({name, index}, project, {variables}) => {
+        if(!variables.value[name]){
+            variables.value[name] = new Variable({name, value: []});
         }
-        return project.variables.value[name].value[index - 1];
+        return variables.value[name].value[index - 1];
     }
 });
 export const add_value_to_list = new Block({
     name: "add_value_to_list",
     template: "{(value) 항목을 (name)에 추가하기}",
-    func: ({name, value}, project) => {
-        if(!project.variables.value[name]){
-            project.variables.value[name] = new Variable({name, value: []});
+    func: ({name, value}, project, {variables}) => {
+        if(!variables.value[name]){
+            variables.value[name] = new Variable({name, value: []});
         }
-        project.variables.value[name].value.push(value);
+        variables.value[name].value.push(value);
     }
 });
 export const remove_value_from_list = new Block({
     pack: basic,
     name: "remove_value_from_list",
     template: "{(index: number) 번째 항목을 (name)에서 삭제하기}",
-    func: ({name, index}, project) => {
-        if(!project.variables.value[name]){
-            project.variables.value[name] = new Variable({name, value: []});
+    func: ({name, index}, project, {variables}) => {
+        if(!variables.value[name]){
+            variables.value[name] = new Variable({name, value: []});
         }
-        project.variables.value[name].value.splice(index - 1, 1);
+        variables.value[name].value.splice(index - 1, 1);
     }
 });
 export const insert_value_to_list = new Block({
     pack: basic,
     name: "insert_value_to_list",
     template: "{(value)을 (name)의 (index: number)번째에 넣기}",
-    func: ({name, index, value}, project) => {
-        if(!project.variables.value[name]){
-            project.variables.value[name] = new Variable({name, value: []});
+    func: ({name, index, value}, project, {variables}) => {
+        if(!variables.value[name]){
+            variables.value[name] = new Variable({name, value: []});
         }
-        project.variables.value[name].value.splice(index - 1, 0, value);
+        variables.value[name].value.splice(index - 1, 0, value);
     }
 });
 export const change_value_list_index = new Block({
     pack: basic,
     name: "change_value_list_index",
     template: "{(name) (index: number)번째 항목을 (value)로 바꾸기}",
-    func: ({name, index, value}, project) => {
-        if(!project.variables.value[name]){
-            project.variables.value[name] = new Variable({name, value: []});
+    func: ({name, index, value}, project, {variables}) => {
+        if(!variables.value[name]){
+            variables.value[name] = new Variable({name, value: []});
         }
-        project.variables.value[name].value[index - 1] = value;
+        variables.value[name].value[index - 1] = value;
     }
 });
 export const length_of_list = new Block({
     pack: basic,
     name: "length_of_list",
     template: "((name) 항목 수): number",
-    func: ({name}, project) => {
-        if(!project.variables.value[name]){
-            project.variables.value[name] = new Variable({name, value: []});
+    func: ({name}, project, {variables}) => {
+        if(!variables.value[name]){
+            variables.value[name] = new Variable({name, value: []});
         }
-        return project.variables.value[name].value.length;
+        return variables.value[name].value.length;
     }
 });
 export const is_included_in_list = new Block({
     name: "is_included_in_list",
     template: "<(name)에 (value)이 포함되어 있는가?>",
-    func: ({name, value}, project) => {
-        if(!project.variables.value[name]){
-            project.variables.value[name] = new Variable({name, value: []});
+    func: ({name, value}, project, {variables}) => {
+        if(!variables.value[name]){
+            variables.value[name] = new Variable({name, value: []});
         }
-        return project.variables.value[name].value.includes(value);
+        return variables.value[name].value.includes(value);
     }
 });
